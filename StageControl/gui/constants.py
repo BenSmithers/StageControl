f0f83e430096e0881a3a067ba96f068febfc3278
Ben Smithers <b.smithers.actual@gmail.com>
2024-10-11 13:43:14 -0700

    added likely USB ports

--- StageControl/gui/constants.py
+++ StageControl/gui/constants.py
@@ -1,9 +1,10 @@
-LED_BOARD_USB = "/dev/tty.usb0"
-STAGE_USB = "/dev/tty.usbFTCHNOSY"
+STAGE_USB = "/dev/tty.usbserial-DK0H5AKJ"
+LED_BOARD_USB = "/dev/tty.usbserial-FTCHNOSY"
 
 HOST = "192.168.11.222"
 USER = "pi"
 PASSWORD = "ADD ME"
+KEY = ""
 PORT = 22
 
 # Maximum pressure before sending an error and locking everything down
