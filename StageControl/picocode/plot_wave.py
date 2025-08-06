a8c7fafab45dafe7a15e278356f98f33732d79c7
bsmithers <bsmithers@triumf.ca>
2025-08-06 18:59:02 +0200

    plot waveforms from hdf5 file

--- StageControl/picocode/plot_wave.py
+++ StageControl/picocode/plot_wave.py
@@ -1,32 +1,63 @@
-from read_pico import PicoMeasure
 import numpy as np 
-import json 
-import time 
-from utils import get_rtime, get_valid, get_cfd_time
 import matplotlib.pyplot as plt 
-print("Initializing")
+import sys 
+
+print("Loading")
 plt.style.use("wms.mplstyle")
 
-test = PicoMeasure(True)
-test.initialze()
-tdiff_bins = np.arange(4, 374, 4)
-rec_bin = np.zeros(len(tdiff_bins)-1)
-mon_bin = np.zeros(len(tdiff_bins)-1)
+grab = 200000
+import h5py as h5 
+from scipy import fftpack
+from scipy import fft
 
-grab = 10000
+data = h5.File(sys.argv[1],'r')
+trig = np.array(data["trigger"])
+monitor = np.array(data["monitor"])
+rec = np.array(data["receiver"])
 
-print("Receiving Waveforms")
-for i in range(10):
-    trig, monitor, rec = test.measure(True)
-    times = np.linspace(0, (test.totalSamples - 1) * test.actualSampleIntervalNs, test.totalSamples)[:grab]
-    plt.clf()
+times = np.array(range(len(rec)))*4
+times = times[:grab]
 
-    
-    plt.plot(times, rec[:grab], label="Receiver")
-    plt.plot(times, monitor[:grab], label="Monitor")
-    plt.plot(times, trig[:grab]/200,label="Trigger/200")
+plt.plot(times, rec[:grab], label="Receiver")
+plt.plot(times, monitor[:grab], label="Monitor")
+plt.plot(times, trig[:grab]/200,label="Trigger/200")
+plt.legend()
+plt.xlabel("Time [ns]")
+plt.ylabel("Amplitude [mV]")
+plt.tight_layout()
+plt.show()
+
+plt.clf()
+
+sample_spacing = (times[1] - times[0])
+print("Spacing", sample_spacing)
+
+freq_space = fft.fft(rec)
+freqs = fftpack.fftfreq(len(rec), sample_spacing)
+
+
+if False:
+    plt.plot(times, monitor, label="Monitor")
+    plt.plot(times, rec, label="Reciever")
+    plt.plot(times, trig, label="Trigger")
     plt.legend()
-    plt.xlabel("Time [ns]")
-    plt.ylabel("Amplitude [mV]")
+    plt.xlabel("Time [ns]", size=14)
+    plt.ylabel("ADC", size=14)
     plt.tight_layout()
     plt.show()
+
+def average(thisdat, nmerge=3):
+    if nmerge==1:
+        return thisdat
+    if int((len(thisdat)%nmerge))!=0:
+        thisdat = thisdat[:-(len(thisdat)%nmerge)]
+    return np.mean(np.reshape(thisdat, (int(len(thisdat)/nmerge), nmerge)), axis=1)
+
+import os 
+plt.plot(average(freqs*1e9, 100), average(np.log10(np.abs(freq_space)), 100) )
+plt.xlim([ -6e6, 6e6])
+plt.ylim([3.2, 5])
+path, fname = os.path.split(sys.argv[1])
+plt.savefig("fourier_{}.png".format(fname), dpi=400)
+
+plt.show()
